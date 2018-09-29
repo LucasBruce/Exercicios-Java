592abb971f82aacd26212d76799ab761f2c95a3d
Lucas Bruce <lucasmatheusbruce@gmail.com>
2018-09-29 20:34:19 -0300

    commit ArmazenandoPolidromos

---
+++ ArmazenandoPolidromos/src/com/polidromo/exercicio/ArmazenandoPolidromos.java
@@ -0,0 +1,34 @@
+package com.polidromo.exercicio;
+/*armazenando numeros, o primeiro elemento do vetorA sera
+ * o ultimo elemento do vetorB e assim por diante*/ 
+import java.util.Scanner;
+
+public class ArmazenandoPolidromos {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+		
+		for(int i = 0; i<vetorA.length; i++) {
+			System.out.println("Entre com o valor na posicao["+(1+i)+"]");
+			vetorA[i] = scan.nextInt();
+			
+			vetorB[vetorA.length - 1 - i] = vetorA[i];
+		}
+		
+		System.out.print("vetorA = ");
+		for(int i = 0; i<vetorA.length; i++) {
+			System.out.print(vetorA[i]+" ");
+		}
+		
+		System.out.println();
+		
+		System.out.print("vetorB = ");
+		for(int i = 0; i<vetorB.length; i++) {
+			System.out.print(vetorB[i]+" ");
+		}
+		
+	}
+
+}
